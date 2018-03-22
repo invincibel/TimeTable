e51b0eaeee71e19143228e25a46dda5e55088a84
invincibel <31722976+invincibel@users.noreply.github.com>
2018-03-22 04:08:26 +0000

    Update home.php

--- home.php
+++ home.php
@@ -12,6 +12,7 @@
 <html>
     <head>
         <title>Home</title>
+	    <link rel="icon" href="logo.png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet'>
         <style>
